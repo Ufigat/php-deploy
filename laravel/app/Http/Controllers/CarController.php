e395ba399427f0e8e9676137690a026ffaaf2302
Kirill Potaralov <k.potaralov@worksolutions.ru>
2023-06-13 18:25:38 +0300

    edit db

--- laravel/app/Http/Controllers/CarController.php
+++ laravel/app/Http/Controllers/CarController.php
@@ -2,17 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\PodcastProcessed;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
+use PDO;
 
 class CarController extends BaseController
 {
     // use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function TestHeandler() : string
+    public function TestHeandler()
     {
-        return "answer";
+//        return ['mysql' => [
+//        'driver' => 'mysql',
+//        'url' => env('DATABASE_URL'),
+//        'host' => env('DB_HOST', '127.0.0.1'),
+//        'port' => env('DB_PORT', '3306'),
+//        'database' => env('DB_DATABASE', 'forge'),
+//        'username' => env('DB_USERNAME', 'forge'),
+//        'password' => env('DB_PASSWORD', ''),
+//        'unix_socket' => env('DB_SOCKET', ''),
+//        'charset' => 'utf8mb4',
+//        'collation' => 'utf8mb4_unicode_ci',
+//        'prefix' => '',
+//        'prefix_indexes' => true,
+//        'strict' => true,
+//        'engine' => null,
+//        'options' => extension_loaded('pdo_mysql') ? array_filter([
+//            PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+//        ]) : [],
+//    ]];
+//        phpinfo();
+        return DB::table('test')->get();
     }
 }
---
+++ laravel/app/Listeners/SendPodcastNotification.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\PodcastProcessed;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class SendPodcastNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\PodcastProcessed  $event
+     * @return void
+     */
+    public function handle(PodcastProcessed $event)
+    {
+       Log::info("hello from send podcastNotification " . $event->name);
+    }
+}
